1e57d46a76549f4e783024ed6ed0e9e9c3e97402
emiqueu <eloya.miqueu@etu.u-bordeaux.fr>
2023-12-10 17:09:30 +0100

    init with old files

---
+++ products.py
@@ -0,0 +1,9 @@
+import json
+
+
+class Products:
+    def __init__(self, name="exemple", price=0, quantity=0, menuQuantity=0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.menuQuantity = menuQuantity
